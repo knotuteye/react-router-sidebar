921cb6814891abed4b8fe946f4a7e7bad6dd0362
Kevin Otuteye <kevinotuteye@gmail.com>
2020-08-27 14:54:28 +0000

    added header

--- src/components/NewSidebar/NewSidebar.js
+++ src/components/NewSidebar/NewSidebar.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './NewSidebar.css'
+import { NavLink } from 'react-router-dom'
 
 export default function ({ sideBarJSON }) {
-  return <div className='sidebar'>
-      
-  </div>
+  return (
+    <div className="sidebar">
+      <NavLink className="sidebar-root" activeClassName="active" to={}>
+        <div>{}</div>
+        <p>{}</p>
+      </NavLink>
+    </div>
+  )
 }
